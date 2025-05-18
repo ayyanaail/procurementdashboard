125af425ebffeaeec068e316ffee3e6588ca45cb
Ayya Naail <naail@redwavemaldives.com>
2025-05-18 15:39:00 +0500

    v3.1

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -38,9 +38,9 @@
                 </div>
 
                 <!-- Sidebar component -->
-                <div class="flex grow flex-col gap-y-5 overflow-y-auto bg-white-900 px-6 pb-4 ring-1 ring-white/10">
-                    <div class="flex h-16 shrink-0 items-center">
-                        <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company">
+                <div class="flex grow flex-col gap-y-5 overflow-y-auto bg-rose-900/90 px-6 pb-4 ring-1 ring-white/10">
+                    <div class="flex flex-col mt-10 h-16 shrink-0 items-center">
+                        <img class="h-8 w-auto" src="{{Vite::asset('resources/images/redwave.png')}}" alt="Your Company">
                     </div>
                     <nav class="flex flex-1 flex-col">
                         <ul role="list" class="flex flex-1 flex-col gap-y-7">
@@ -54,13 +54,19 @@
                                             </svg>
                                             Dashboard
                                         </x-nav-link>
+                                        <x-nav-link :href="route('orders.index')" :active="request()->routeIs('dashboard')"
+                                                    class="text-gray-400 hover:text-white hover:bg-gray-800 group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold">
+                                            <svg class="h-6 w-6 shrink-0" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
+                                            </svg>
+                                            Manage Orders
+                                        </x-nav-link>
                                     </li>
                                 </ul>
                             </li>
                             <li class="mt-auto">
                                 <div x-data="{ open: false }" class="relative">
-                                    <button @click="open = !open" type="button" class="flex items-center gap-x-4 w-full rounded-md p-2 text-sm font-semibold leading-6 text-white hover:bg-gray-800">
-                                        <img class="h-8 w-8 rounded-full bg-gray-800" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+                                    <button @click="open = !open" type="button" class="flex items-center gap-x-4 w-full rounded-md p-2 text-sm font-semibold leading-6 text-white hover:bg-rose-900 hover:ring-1 hover:ring-white">
                                         <span class="sr-only">Your profile</span>
                                         <span aria-hidden="true">{{ Auth::user()->name }}</span>
                                     </button>
@@ -174,13 +180,13 @@
     </div>
 
     <!-- Mobile menu button -->
-    <div class="sticky top-0 z-40 flex items-center gap-x-6 bg-gray-900 px-4 py-4 shadow-sm sm:px-6 lg:hidden">
+    <div class="sticky top-0 z-40 flex items-center gap-x-6 bg-gradient-to-r from-rose-600 to-red-500 px-4 py-4 shadow-sm sm:px-6 lg:hidden">
         <button type="button" @click="open = true" class="-m-2.5 p-2.5 text-gray-400 lg:hidden">
             <span class="sr-only">Open sidebar</span>
             <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
             </svg>
         </button>
-        <div class="flex-1 text-sm font-semibold leading-6 text-white">Dashboard</div>
+        <div class="flex-1 text-sm font-semibold leading-6 text-white">RedWave Procurement Dashboard</div>
     </div>
 </div>
