1e5d513540b60da644a2f771251bd3ee71c1638a
Ayya Naail <naail@redwavemaldives.com>
2025-05-18 20:32:13 +0500

    4.1.1

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -68,7 +68,10 @@
                                 <div x-data="{ open: false }" class="relative">
                                     <button @click="open = !open" type="button" class="flex items-center gap-x-4 w-full rounded-md p-2 text-sm font-semibold leading-6 text-white hover:bg-rose-900 hover:ring-1 hover:ring-white">
                                         <span class="sr-only">Your profile</span>
-                                        <span aria-hidden="true">{{ Auth::user()->name }}</span>
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 h-5">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                        </svg>
+                                        <span aria-hidden="true">{{ Auth::user()->name }} - {{Auth::user()->email}}</span>
                                     </button>
 
                                     <div x-show="open"
